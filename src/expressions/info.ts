efde37b2b497fa85c04fdc4f9ecfac20818bc880
Fran González <fgonzaleva@icloud.com>
2019-11-14 20:26:56 +0100

    :recycle: Refactor info.ts to remove duplicated code

--- src/expressions/info.ts
+++ src/expressions/info.ts
@@ -1,4 +1,4 @@
-import Step from '../types/step'
+import Step, { ValidationExpression } from '../types/step'
 import Info from '../types/expressions/info'
 import { Input } from '../types/input'
 import {
@@ -10,6 +10,7 @@ import {
 import createValidation from '../validation'
 import Validator from '../types/expressions/validator'
 import createSyntax from '../syntax'
+import ValidatorSyntax from '../types/syntax'
 
 const createValidationOf = <T, P>(
     input: Input<T, P>,
@@ -20,60 +21,51 @@ const createValidationOf = <T, P>(
         input.propertyName as keyof T,
     )
 
+/**
+ * Updates the last step of a given list of steps if possible applying the given
+ * updater function.
+ *
+ * @param input Current input of the validation.
+ * @param steps List of the steps until now.
+ * @param updater Function to apply to the last element, if possible.
+ */
+const updateLastStepWithInfo = <T, P>(
+    input: Input<T, P>,
+    steps: Step[],
+    updater: (expression: ValidationExpression) => ValidationExpression,
+): ValidatorSyntax<Validator<T, P>, T, P> => {
+    const clonedSteps = [...steps]
+    const lastStep = clonedSteps.pop()
+    if (!lastStep) return createSyntax(createValidationOf, input, steps)
+    if (!isValidationExpression(lastStep.expression))
+        return createSyntax(createValidationOf, input, steps)
+
+    const updatedSteps = [
+        ...clonedSteps,
+        {
+            ...lastStep,
+            expression: updater(lastStep.expression),
+        },
+    ]
+
+    return createSyntax(createValidationOf, input, updatedSteps)
+}
+
 const createInfo = <T, P>(input: Input<T, P>, steps: Step[]): Info<T, P> => ({
-    withPropertyName(propertyName) {
-        const clonedSteps = [...steps]
-        const lastStep = clonedSteps.pop()
-        if (!lastStep) return createSyntax(createValidationOf, input, steps)
-        if (!isValidationExpression(lastStep.expression))
-            return createSyntax(createValidationOf, input, steps)
+    withPropertyName: propertyName =>
+        updateLastStepWithInfo(input, steps, expression =>
+            updatePropertyName(expression, propertyName),
+        ),
 
-        const updatedSteps = [
-            ...clonedSteps,
-            {
-                ...lastStep,
-                expression: updatePropertyName(
-                    lastStep.expression,
-                    propertyName,
-                ),
-            },
-        ]
+    withCode: code =>
+        updateLastStepWithInfo(input, steps, expression =>
+            updateCode(expression, code),
+        ),
 
-        return createSyntax(createValidationOf, input, updatedSteps)
-    },
-
-    withCode(code) {
-        const clonedSteps = [...steps]
-        const lastStep = clonedSteps.pop()
-        if (!lastStep) return createSyntax(createValidationOf, input, steps)
-        if (!isValidationExpression(lastStep.expression))
-            return createSyntax(createValidationOf, input, steps)
-
-        const updatedSteps = [
-            ...clonedSteps,
-            { ...lastStep, expression: updateCode(lastStep.expression, code) },
-        ]
-
-        return createSyntax(createValidationOf, input, updatedSteps)
-    },
-
-    withMessage(message) {
-        const clonedSteps = [...steps]
-        const lastStep = clonedSteps.pop()
-        if (!lastStep) return createSyntax(createValidationOf, input, steps)
-        if (!isValidationExpression(lastStep.expression))
-            return createSyntax(createValidationOf, input, steps)
-
-        const updatedSteps = [
-            ...clonedSteps,
-            {
-                ...lastStep,
-                expression: updateMessage(lastStep.expression, message),
-            },
-        ]
-
-        return createSyntax(createValidationOf, input, updatedSteps)
-    },
+    withMessage: message =>
+        updateLastStepWithInfo(input, steps, expression =>
+            updateMessage(expression, message),
+        ),
 })
 
 export default createInfo
